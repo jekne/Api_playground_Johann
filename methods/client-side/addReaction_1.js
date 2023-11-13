16d5249eab65ec543db58c6487159cfe877c361d
jekne <jekneto@hotmail.com>
2023-11-13 14:18:54 +0100

    track

--- methods/client-side/addReaction_1.js
+++ methods/client-side/addReaction_1.js
@@ -1,25 +1,25 @@
-import { chatClient } from "./client.js";
+// import { chatClient } from "./client.js";
 
-const addReactions = async (messageID) => {
-  try {
-    const channel = chatClient.channel("messaging", "testEvents", {});
+// const addReactions = async (messageID) => {
+//   try {
+//     const channel = chatClient.channel("messaging", "testEvents", {});
 
-    // Add reaction 'like' and replace all other reactions of this user by it
-    const reaction = await channel.sendReaction(
-      messageID,
-      {
-        type: "love",
-      },
-      null,
-      true
-    );
+//     // Add reaction 'like' and replace all other reactions of this user by it
+//     const reaction = await channel.sendReaction(
+//       messageID,
+//       {
+//         type: "love",
+//       },
+//       null,
+//       true
+//     );
 
-    return reaction;
-  } catch (error) {
-    console.log("error >>>", error);
-  }
-};
+//     return reaction;
+//   } catch (error) {
+//     console.log("error >>>", error);
+//   }
+// };
 
-addReactions("5cc0f194-93b5-443f-b7f5-d8c46ecdcbce").then((r) =>
-  console.log(r)
-);
+// addReactions("5cc0f194-93b5-443f-b7f5-d8c46ecdcbce").then((r) =>
+//   console.log(r)
+// );
